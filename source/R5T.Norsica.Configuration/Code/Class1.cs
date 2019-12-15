996ac3611f11970c99bb14da4268574e4f88a93d
MinexAutomation <minex.automation@gmail.com>
2019-12-14 22:54:16 -0800

    Added INugetExecutableFilePathProvider service definition interface type.

--- source/R5T.Norsica.Configuration/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Norsica.Configuration
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Norsica.Configuration/Code/Services/Definitions/INugetExecutableFilePathProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Norsica.Configuration
+{
+    public interface INugetExecutableFilePathProvider
+    {
+        string GetNugetExecutableFilePath();
+    }
+}
